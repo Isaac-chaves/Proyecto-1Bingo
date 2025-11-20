ea80bdce8c373c90d735aa63a564bc12bc241ab5
Yefran <yechaverrimi@est.utn.ac.cr>
2025-11-19 21:46:37 -0600

    Class TableroNumeros
    Se crea TableroNumeros, Esta clase administra
    los números cantados durante la partida, evita duplicados y registra el
    último número ingresado. Incluye métodos para agregar números y reiniciar
    el tablero.

---
+++ src/modelo/TableroNumeros.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author UTN
+ */
+public class TableroNumeros {
+
+    private static TableroNumeros instancia;
+
+    private Set<Integer> numerosCantados = new HashSet<>();
+    private Integer ultimoNumero = null;
+
+    private TableroNumeros() {
+    }
+
+    public static TableroNumeros getInstancia() {
+        if (instancia == null) {
+            instancia = new TableroNumeros();
+        }
+        return instancia;
+    }
+
+    public boolean agregarNumero(int n) {
+        if (n < 1 || n > 75) {
+            return false;
+        }
+        if (numerosCantados.contains(n)) {
+            return false;
+        }
+
+        numerosCantados.add(n);
+        ultimoNumero = n;
+        return true;
+    }
+
+    public void reiniciar() {
+        numerosCantados.clear();
+        ultimoNumero = null;
+    }
+
+    public Integer getUltimoNumero() {
+        return ultimoNumero;
+    }
+} 
