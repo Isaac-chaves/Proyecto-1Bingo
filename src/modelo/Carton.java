e8ab9a16ad5d2474a056b35218498e613f349429
Yefran <yechaverrimi@est.utn.ac.cr>
2025-11-19 21:44:36 -0600

    Class carton
    Se crea Carton, encargada de representar un cartón
    de Bingo. Almacena la matriz de números (5x5), la matriz de casillas
    marcadas y permite marcar/desmarcar números. La casilla central se marca
    automáticamente como "libre". Esta clase es parte principal del modelo
    del juego.

---
+++ src/modelo/Carton.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author UTN
+ */
+public class Carton {
+
+    private String id;
+    private int[][] numeros;
+    private boolean[][] marcados;
+
+    public Carton(String id, int[][] numeros) {
+        this.id = id;
+        this.numeros = numeros;
+
+        this.marcados = new boolean[5][5];
+
+        marcados[2][2] = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int[][] getNumeros() {
+        return numeros;
+    }
+
+    public boolean[][] getMarcados() {
+        return marcados;
+    }
+
+    public void marcarNumero(int n) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (numeros[i][j] == n) {
+                    marcados[i][j] = true;
+                }
+            }
+        }
+    }
+
+    public void desmarcarNumero(int n) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (numeros[i][j] == n) {
+                    marcados[i][j] = false;
+                }
+            }
+        }
+        marcados[2][2] = true;
+    }
+} 
